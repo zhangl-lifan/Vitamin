5b1304060ddfcd5fcde7753e91f54fef88f93f8d
王子龙 <wangzilong@wangzilong.local>
2019-10-16 14:53:16 +0800

    搜索

--- src/api/customer.js
+++ src/api/customer.js
@@ -2,7 +2,7 @@
  * @Description: In User Settings Edit
  * @Author: your name
  * @Date: 2019-10-15 14:19:16
- * @LastEditTime: 2019-10-15 14:42:56
+ * @LastEditTime: 2019-10-16 14:06:58
  * @LastEditors: Please set LastEditors
  */
 import request from '@/utils/request'
@@ -12,6 +12,17 @@ export function member(data) {
   data = qs.stringify(data)
   return request({
     url: '/member/member-list',
+    method: 'post',
+    baseURL: '/api',
+    data
+  })
+}
+
+export function searchset(data) {
+  // console.log('data',data);
+  data = qs.stringify(data)
+  return request({
+    url: '/member/search-set',
     method: 'post',
     baseURL: '/api',
     data
